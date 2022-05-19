69bb808e330bb5a5ef66fff27f87264369b941c4
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-19 10:49:36 -0500

    Create Banks admin page

---
+++ src/pages/Admin/Banks/Banks.js
@@ -0,0 +1,48 @@
+import React, { useContext, useEffect } from "react";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import TableBanks from "../../../components/Admin/Banks/TableBanks";
+import InputSearchBank from "../../../components/Admin/Banks/InputSearchBanks";
+import { AdminContext } from "../../../providers/AdminProvider";
+import { getBanksApi } from "../../../api/banks";
+import { ProgressCircular } from "../../../components/Others/ProgressCircular";
+import { EmptyInfo } from "../../../components/Others/EmptyInfo";
+
+const Banks = () => {
+  const { banks, setBanks } = useContext(AdminContext);
+  useEffect(() => {
+    (async () => {
+      const response = await getBanksApi();
+      if (response.ok) {
+        setBanks(response.data);
+      }
+    })();
+  }, [setBanks]);
+  return (
+    <>
+      <Typography align="center" variant="h2" color="initial">
+        Lista de Bancos
+      </Typography>
+      <Container component="div" maxWidth="lg">
+        {!banks ? (
+          <ProgressCircular variantMessage="h4" message="Cargando bancos..." />
+        ) : banks.length > 0 ? (
+          <>
+            <InputSearchBank />
+            <TableBanks />
+          </>
+        ) : (
+          <>
+            <InputSearchBank />
+            <EmptyInfo
+              title="No hay registros de bancos aún"
+              variantMessage="h4"
+            />
+          </>
+        )}
+      </Container>
+    </>
+  );
+};
+
+export default Banks;
