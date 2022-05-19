5dac4526e97caa95a0503b1feb735c80e00769e2
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-19 10:48:26 -0500

    Use AdminProvider

--- src/App.js
+++ src/App.js
@@ -6,16 +6,19 @@ import MomentUtils from "@date-io/moment";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import "moment/locale/es";
 import AuthProvider from "./providers/AuthProvider";
+import AdminProvider from "./providers/AdminProvider";
 import { AppRouter } from "./routes/AppRouter";
 
 function App() {
   return (
     <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <MuiPickersUtilsProvider utils={MomentUtils} locale="es">
-         <AppRouter />
-        </MuiPickersUtilsProvider>
-      </ThemeProvider>
+      <AdminProvider>
+        <ThemeProvider theme={theme}>
+          <MuiPickersUtilsProvider utils={MomentUtils} locale="es">
+            <AppRouter />
+          </MuiPickersUtilsProvider>
+        </ThemeProvider>
+      </AdminProvider>
     </AuthProvider>
   );
 }
