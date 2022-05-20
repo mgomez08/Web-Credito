9506f15127bfacb4791b2df72746a02ff06a36ae
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-20 10:46:29 -0500

    Add Services routes on AdminRouter

--- src/routes/routers/AdminRouter.js
+++ src/routes/routers/AdminRouter.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CreateBank from "../../components/Admin/Banks/CreateBank";
 import EditBank from "../../components/Admin/Banks/EditBank";
+import CreateService from "../../components/Admin/Services/CreateService";
+import EditService from "../../components/Admin/Services/EditService";
 import Banks from "../../pages/Admin/Banks/Banks";
 import Dashboard from "../../pages/Admin/Dashboard/Dashboard";
+import Services from "../../pages/Admin/Services/Services";
 
 export const AdminRouter = () => {
   return (
@@ -13,6 +16,17 @@ export const AdminRouter = () => {
         <Route exact path="/dashboard/banks" component={Banks} />
         <Route exact path="/dashboard/banks/create" component={CreateBank} />
         <Route exact path="/dashboard/banks/edit/:id" component={EditBank} />
+        <Route exact path="/dashboard/services" component={Services} />
+        <Route
+          exact
+          path="/dashboard/services/create"
+          component={CreateService}
+        />
+        <Route
+          exact
+          path="/dashboard/services/edit/:id"
+          component={EditService}
+        />
       </Switch>
     </>
   );
