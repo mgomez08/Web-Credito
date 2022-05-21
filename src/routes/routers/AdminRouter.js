f9acc12721e3706e399bff796a49fb3ef3d6a827
Marlon Gómez <marlonjulian15@gmail.com>
2022-05-21 12:51:16 -0500

    Add interests pages on adminrouter

--- src/routes/routers/AdminRouter.js
+++ src/routes/routers/AdminRouter.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import CreateBank from "../../components/Admin/Banks/CreateBank";
 import EditBank from "../../components/Admin/Banks/EditBank";
+import CreateInterest from "../../components/Admin/Interests/CreateInterest";
+import EditInterest from "../../components/Admin/Interests/EditInterest";
 import CreateService from "../../components/Admin/Services/CreateService";
 import EditService from "../../components/Admin/Services/EditService";
 import Banks from "../../pages/Admin/Banks/Banks";
 import Dashboard from "../../pages/Admin/Dashboard/Dashboard";
+import Interests from "../../pages/Admin/Interests/Interests";
 import Services from "../../pages/Admin/Services/Services";
 
 export const AdminRouter = () => {
@@ -26,6 +29,17 @@ export const AdminRouter = () => {
           exact
           path="/dashboard/services/edit/:id"
           component={EditService}
+        />
+        <Route exact path="/dashboard/interests" component={Interests} />
+        <Route
+          exact
+          path="/dashboard/interests/create"
+          component={CreateInterest}
+        />
+        <Route
+          exact
+          path="/dashboard/interests/edit/:id"
+          component={EditInterest}
         />
       </Switch>
     </>
